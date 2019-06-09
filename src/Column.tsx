b5f3721f72dce25f470ae12df5c8d32eef181dbe
Sivakar Sithamparanathan <sivakar12@outlook.com>
2019-06-10 02:34:18 +0530

    Change column view to highlight selected item

--- src/Column.tsx
+++ src/Column.tsx
@@ -14,12 +14,15 @@ const Column: FunctionComponent<ColumnDetails> = (props) => {
     function handleClickItem(item: string) {
         state.changePath(_.concat(props.path, [item]))
     }
+    let selectedItem: string = ''
+    if (state.path.length > props.level + 1)
+        selectedItem = state.path[props.level + 1]
     return (
         <div className="column">
             <div className="column-title">{_.last(props.path)}</div>
             <div className="items">
                 {props.content.map((item, index) => 
-                    <div className="item" onClick={() => { handleClickItem(item) }}>{item}</div>
+                    <div className={item == selectedItem? "item-selected" : "item"} onClick={() => { handleClickItem(item) }}>{item}</div>
                 )}
             </div>
         </div>
